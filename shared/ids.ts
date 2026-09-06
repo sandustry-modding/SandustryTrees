59a9be4218c8526cebb9efbd6375b531b7b902f1
IrishBruse <econn50@outlook.com>
2026-09-06 14:46:49 +0100

    Lint

--- shared/ids.ts
+++ shared/ids.ts
@@ -2,33 +2,35 @@ import { modinfo } from "../modinfo.ts";
 
 const root = modinfo.id;
 
-function ns(test: string) {
-  return `${root}:${test}`;
-}
-
-function key(test: string) {
-  return `${root}.${test}`;
-}
+const ns = `${root}:`;
+const key = `${root}:`;
 
 export const ELEMENT = {
-  pineCone: ns("pineCone"),
-  pineShoot: ns("pineShoot"),
-  pineNeedle: ns("pineNeedle"),
-  wood: ns("wood"),
-  charcoal: ns("charcoal"),
-  compost: ns("compost"),
-  wetCompost: ns("wetCompost"),
+  pineCone: ns + "pineCone",
+  pineShoot: ns + "pineShoot",
+  pineNeedle: ns + "pineNeedle",
+  acorn: ns + "acorn",
+  oakShoot: ns + "oakShoot",
+  oakLeaf: ns + "oakLeaf",
+  wood: ns + "wood",
+  charcoal: ns + "charcoal",
+  compost: ns + "compost",
+  wetCompost: ns + "wetCompost",
 } as const;
 
 export const NAME_KEY = {
-  pineCone: key("pineCone.name"),
-  pineShoot: key("pineShoot.name"),
-  pineNeedle: key("pineNeedle.name"),
-  pineWood: key("pineWood.name"),
-  wood: key("wood.name"),
-  charcoal: key("charcoal.name"),
-  compost: key("compost.name"),
-  wetCompost: key("wetCompost.name"),
+  pineCone: key + "pineCone.name",
+  pineShoot: key + "pineShoot.name",
+  pineNeedle: key + "pineNeedle.name",
+  pineWood: key + "pineWood.name",
+  acorn: key + "acorn.name",
+  oakShoot: key + "oakShoot.name",
+  oakLeaf: key + "oakLeaf.name",
+  oakWood: key + "oakWood.name",
+  wood: key + "wood.name",
+  charcoal: key + "charcoal.name",
+  compost: key + "compost.name",
+  wetCompost: key + "wetCompost.name",
 } as const;
 
 export const VANILLA_ELEMENT = {
@@ -37,13 +39,14 @@ export const VANILLA_ELEMENT = {
 } as const;
 
 export const TERRAIN = {
-  pineWood: ns("pineWood"),
+  pineWood: ns + "pineWood",
+  oakWood: ns + "oakWood",
 } as const;
 
 export const STRUCTURE = {
-  sieve: ns("sieve"),
+  sieve: ns + "sieve",
 } as const;
 
 export const SPRITE = {
-  sieve: ns("sieve-sprite"),
+  sieve: ns + "sieve-sprite",
 } as const;
